e2113e5c64a5b5265df2bf5bba9f692151c9d036
sulimanalhassan123as-code <sulimanalhassan.123.as@gmail.com>
2025-10-14 16:49:08 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
@@ -12,13 +10,10 @@ const firebaseConfig = {
   storageBucket: "never-hide-recorder.appspot.com",
   messagingSenderId: "1013655836375",
   appId: "1:1013655836375:web:f77bed8ca8e8f0e1e58292",
-  measurementId: "G-2478D7MRP6"
+  measurementId: "G-2478D7MRP6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-export { auth, storage };
+export const storage = getStorage(app);
+export { ref, uploadBytes, getDownloadURL, listAll };
